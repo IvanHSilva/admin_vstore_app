a10d63fd249cdfb37358c0947b9253aa600ee72f
Ivan Henriques da Silva <ivanhenriquessilva@gmail.com>
2020-10-10 12:29:09 -0300

    Login System

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  PageController _pageController;
+  int _page = 0;
+
+  @override initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[850],
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: Colors.pinkAccent,
+          primaryColor: Colors.white,
+          textTheme: Theme.of(context).textTheme.copyWith(
+            caption: TextStyle(color: Colors.white54),
+          ),
+        ),
+        child: BottomNavigationBar(
+          currentIndex: _page,
+          onTap: (p) {
+            _pageController.animateToPage(
+              p, 
+              duration: Duration(milliseconds: 500), 
+              curve: Curves.ease
+              );
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              title: Text('Clientes'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart),
+              title: Text('Pedidos'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.list),
+              title: Text('Produtos'),
+            ),
+          ],
+        ),
+      ),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (p) {
+          setState(() {
+            _page = p;
+          });
+        },
+        children: [
+          Container(color: Colors.red,),
+          Container(color: Colors.yellow,),
+          Container(color: Colors.green,),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,9 @@
-import 'package:admin_vstore/widgets/input_field.dart';
 import 'package:flutter/material.dart';
+import 'package:admin_vstore/blocs/login_bloc.dart';
+import 'package:admin_vstore/widgets/input_field.dart';
+
+import 'home_screen.dart';
+//import 'package:admin_vstore/screens/home_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -7,51 +11,117 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+
+  final _loginBloc = LoginBloc();
+
+  @override
+  void initState() {
+    super.initState();
+    _loginBloc.outState.listen((state) {
+      switch(state) {
+        case LoginState.SUCCESS:
+          Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => HomeScreen())
+          );
+        break;
+        case LoginState.FAIL:
+          showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+              title: Text('Erro'),
+              content: Text('Você não possui os privilégios necessários!'),
+            ));
+          break;
+        case LoginState.LOADING:
+        case LoginState.IDLE:
+      }
+     });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[850],      
-      body: Stack(
-        alignment: Alignment.center,
-        children: [
-          Container(),
-          SingleChildScrollView(
-            child: Container(
-              margin: EdgeInsets.all(16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
+      body: StreamBuilder<LoginState>(
+        stream: _loginBloc.outState,
+        initialData: LoginState.LOADING,
+        builder: (context, snapshot) {
+          print(snapshot.data);
+          switch(snapshot.data) {
+            case LoginState.LOADING:
+              return Center(
+                child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation(
+                    Colors.pinkAccent),
+                  ),
+                );
+              break;
+            case LoginState.FAIL:
+            case LoginState.SUCCESS:
+            case LoginState.IDLE:
+              return Stack(
+                alignment: Alignment.center,
                 children: [
-                  Icon(
-                    Icons.store_mall_directory,
-                    color: Colors.pinkAccent,
-                    size: 160,
-                  ),
-                  InputField(
-                    icon: Icons.person_outline,
-                    hint: 'Usuário',
-                    obscure: false,
-                  ),
-                  InputField(
-                    icon: Icons.lock_outline,
-                    hint: 'Senha',
-                    obscure: true,
-                  ),
-                  SizedBox(height: 32,),
-                  SizedBox(
-                    height: 50,
-                    child: RaisedButton(
-                      color: Colors.pinkAccent,
-                      child: Text('Entrar'),
-                      onPressed: (){},
-                      textColor: Colors.white,
+                  Container(),
+                  SingleChildScrollView(
+                    child: Container(
+                      margin: EdgeInsets.all(16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Icon(
+                            Icons.store_mall_directory,
+                            color: Colors.pinkAccent,
+                            size: 160,
+                          ),
+                          InputField(
+                            icon: Icons.person_outline,
+                            hint: 'Usuário',
+                            obscure: false,
+                            stream: _loginBloc.outEmail,
+                            onChanged: _loginBloc.changeEmail,
+                          ),
+                          InputField(
+                            icon: Icons.lock_outline,
+                            hint: 'Senha',
+                            obscure: true,
+                            stream: _loginBloc.outPassword,
+                            onChanged: _loginBloc.changePassword,
+                          ),
+                          SizedBox(height: 32,),
+                          StreamBuilder<bool>(
+                            stream: _loginBloc.outSubmitValid,
+                            builder: (context, snapshot) {
+                              return SizedBox(
+                                height: 50,
+                                child: RaisedButton(
+                                  color: Colors.pinkAccent,
+                                  child: Text('Entrar'),
+                                  onPressed: snapshot.hasData ? _loginBloc.submit : null,
+                                  textColor: Colors.white,
+                                  disabledColor: Colors.pinkAccent.withAlpha(140),
+                                ),
+                              );
+                            }
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ],
-              ),
-            ),
-          ),
-        ]
+                ]
+              );
+              break;
+            default:
+            return Container();
+          }
+        }
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _loginBloc.dispose();
+    super.dispose();
   }
 }
